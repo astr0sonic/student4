5cc50215978c1aa61b54cda565e10db5b8686df7
Aminulka <urntbuzinga@gmail.com>
2024-05-17 18:37:44 +0300

    taskk3

---
+++ sundaram.cpp
@@ -0,0 +1,18 @@
+#include "sundaram.h"
+#include "vector"
+
+std::vector<int> sieve(int n) {
+    std::vector<int> primes;
+    primes.push_back(2);
+
+    vector <int> arr(n + 1);
+    for (int i = 2; i <= n; i++) arr[i] = i;
+
+    for (int i = 1; i <= n; i++) for (int j = 1; j <= i; j++) if (i + j + 2 * i * j <= n) arr[i + j + 2 * i * j] = 0;
+    int i = 1;
+    while (2 * i + 1 <= n) {
+        if (arr[i] != 0) cout << primes.push_back(2 * i + 1);
+        i++;
+    }
+    return primes;
+}
